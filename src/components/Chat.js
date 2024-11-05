d26fd772bd457087884a8e067649862ab54b52da
Charan Teja <charanteja007tuf@outlook.com>
2024-11-05 11:37:07 +0530

    Update Chat.js

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -448,13 +448,13 @@ const ChatRoom = () => {
               <strong>{msg.userName} uploaded:</strong>
               <br />
               {msg.file.startsWith('data:audio/') && (
-                <audio controls style={{width: '-webkit-fill-available'}}>
+                <audio style={{width: '-webkit-fill-available'}}>
                   <source src={msg.file} type={msg.file.type} />
                   Your browser does not support the audio tag.
                 </audio>
               )}
               {msg.file.startsWith('data:video/') && (
-                <video controls style={{ width: '100%' }}>
+                <video style={{ width: '100%' }}>
                   <source src={msg.file} type={msg.file.type} />
                   Your browser does not support the video tag.
                 </video>
