e61d42455bc22b6359a2712ed64d32872aafea9f
Charan Teja <charanteja007tuf@outlook.com>
2025-09-08 13:40:03 +0530

    Update FileUpload.js

--- src/components/FileUpload.js
+++ src/components/FileUpload.js
@@ -16,7 +16,7 @@ const UploadContainer = styled(motion.div)`
 
 const FileUpload = () => {
   const handleFileChange = (e) => {
-    console.log(e.target.files[0]);//
+   // console.log(e.target.files[0]);
   };
 
   return (
